05f5582ecccab053b81308f8d825f2ffc8d0db46
kapturoff <pigunderwater@gmail.com>
2020-11-08 23:33:28 +0300

    Add interfaces for request handlers

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -7,3 +7,28 @@ export type Pair = {
 	santa: User
 	recipient: User
 }
+
+export type ClientInfo = {
+	button_actions: string[]
+	keyboard: boolean
+	inline_keyboard: boolean
+	carousel: boolean
+	lang_id: number
+}
+
+export type MessageResponse = {
+	text: string
+	buttons?: {
+		color: 'positive' | 'negative' | 'secondary' | 'primary'
+		action: {
+			label: string
+			payload: object
+			type?: 'text'
+		}
+	}[]
+}
+
+export type Room = {
+	code: string
+	name: string
+}
