b1a7b1104cd5bc16edd93d0db2a9dfcff2c2648a
kapturoff <pigunderwater@gmail.com>
2020-12-08 20:47:38 +0300

    Add request helper for started game

---
+++ src/helpers/requestHandlers/roomGameStarted.ts
@@ -0,0 +1,20 @@
+import { ClientInfo, MessageResponse, Room, User } from 'interfaces'
+
+export default function roomNewParticipant(receiver: User, room: Room): MessageResponse {
+	return {
+		text: `Игра в комнате "${room.name}" (${room.code}) началась! 
+Человек, которому ты должен подарить подарок: ${receiver.first_name} ${
+			receiver.last_name
+		} (vk.com/id${receiver.id}).
+${
+	receiver.wishlist
+		? `\nВот что он${receiver.sex === 1 ? 'а' : 'о'} написал${
+				receiver.sex === 1 ? 'а' : 'о'
+		  } в своём списке желаний: \n${receiver.wishlist}`
+		: ''
+}
+
+Желаю вам всем хорошо провести время!
+`,
+	}
+}
